178483aaed41424797422751010eecd0d0f54bdf
swaranjali-patil <156639603+swaranjali-patil@users.noreply.github.com>
2024-07-04 16:35:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-let nav=document.querySelector('nav');
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
 
-window.addEventListener('scroll',()=>{
-    nav.classList.toggle('naWinScroll',window,scrollY>0)
-})
+const calculate = (btnValue) => {
+    if(btnValue === "=" && btnValue !==""){
+        output = eval(output.replace("%","/100"));
+    }else if(btnValue ==="AC"){
+        output = "";
+    }else if(btnValue ==="DEL"){
+        output = output.toString().slice(0,-1);
+    }else{
+        if(output === "" && specialChars.includes(btnValue)) return;
+        output += btnValue;
+    }
+    display.value = output;
+
+};
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+
+});
